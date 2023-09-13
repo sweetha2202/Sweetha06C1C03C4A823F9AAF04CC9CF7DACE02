43444a2d0089b21b3d2bda2d36cddf2d4bc44969
sweetha2202 <144819557+sweetha2202@users.noreply.github.com>
2023-09-13 19:43:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0):
+        self._account_number = account_number
+        self._account_holder_name = account_holder_name
+        self._account_balance = initial_balance
 
+    def deposit(self, amount):
+        if amount > 0:
+            self._account_balance += amount
+            return f"Deposited ${amount}. New balance: ${self._account_balance}"
+        else:
+            return "Invalid deposit amount. Please enter a positive number."
 
-def factorial(x):
-    """This is a recursive function
-    to find the factorial of an integer"""
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self._account_balance:
+            self._account_balance -= amount
+            return f"Withdrew ${amount}. New balance: ${self._account_balance}"
+        elif amount <= 0:
+            return "Invalid withdrawal amount. Please enter a positive number."
+        else:
+            return "Insufficient funds for withdrawal."
 
-    if x == 1:
-        return 1
-    else:
-        
-        return (x * factorial(x-1))
+    def display_balance(self):
+        return f"Account Balance for {self._account_holder_name}: ${self._account_balance}"
 
-
-
-num = 7
-
-
-result = factorial(num)
-print("The factorial of", num, "is", result)
+my_account = BankAccount("12345", "John Doe", 1000)
+print(my_account.display_balance()) 
+print(my_account.deposit(500))      
+print(my_account.withdraw(200))     
+print(my_account.display_balance())  
